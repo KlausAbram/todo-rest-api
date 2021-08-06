15809b149cfcc2c59e496aa77448f369364319ee
KlausAbram <sf1951@mail.ru>
2021-08-06 16:14:26 +0300

    Implementation of dependencies && Onion-architecture && Injections

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,17 @@ package main
 import (
 	todoapi "github.com/klaus-abram/todo-rest-api"
 	"github.com/klaus-abram/todo-rest-api/pkg/handler"
+	"github.com/klaus-abram/todo-rest-api/pkg/repository"
+	"github.com/klaus-abram/todo-rest-api/pkg/service"
 	"log"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+
+	repo := repository.NewRepository()
+	services := service.NewService(repo)
+	handlers := handler.NewHandler(services)
+
 	serv := new(todoapi.Server)
 	if errRun := serv.RunServer("8000", handlers.InitRoutes()); errRun != nil {
 		log.Fatalf("error with the running the http server %s", errRun.Error())
--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -1,8 +1,16 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/klaus-abram/todo-rest-api/pkg/service"
+)
 
 type Handler struct {
+	services *service.Service
+}
+
+func NewHandler(services *service.Service) *Handler {
+	return &Handler{services: services}
 }
 
 func (hnd *Handler) InitRoutes() *gin.Engine {
